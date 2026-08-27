ac34ebbf62ab6ae23af1783ae24ad754717854a0
Khushboo Yadav <khushi901359@gmail.com>
2026-08-27 20:45:57 +0530

    Refactor EC2 instance and security group configurations

--- module-postgres/ec2_instance.tf
+++ module-postgres/ec2_instance.tf
@@ -1,77 +1,42 @@
-# key pair(login)
-
-data "aws_key_pair" "my_ec2_key"{
-
-  key_name   =var.ec2_key_name
-  
+# Key Pair
+data "aws_key_pair" "my_ec2_key" {
+  key_name = var.ec2_key_name
 }
 
-# vpc and security group
+# Default VPC
 resource "aws_default_vpc" "default" {
-
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "k8s-automate-sg"
-  description = "Security group for k8s cluster with argocd and postgreSQL"
-  vpc_id      = aws_default_vpc.default.id #interpolation
-  # inbound rule
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description ="SSH Access"
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description ="HTTP Access"
-  }
-  ingress{
-    from_port   =443
-    to_port     =443
-    protocol    ="tcp"
-    cidr_blocks =["0.0.0.0/0"]
-    description ="HTTPS Access (Argocd web ui)"
-  }
-  ingress{
-    from_port   =6443
-    to_port     =6443
-    protocol    ="tcp"
-    cidr_blocks =["0.0.0.0/0"]
-    description ="k8s API server"
-   }
-
-  #outbound rule
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    name = "k8s-autoamte-sg"
+# Existing Security Group
+data "aws_security_group" "allow_ssh" {
+  filter {
+    name   = "group-name"
+    values = ["k8s-automate-sg"]
   }
 
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default.id]
+  }
 }
-# EC2 instance 
+
+# EC2 Instance
 resource "aws_instance" "my_ec2" {
-  count           =var.instance_count
-  depends_on      = [aws_security_group.allow_ssh]
-  ami             = var.ec2_ami_id
-  key_name        = data.aws_key_pair.my_ec2_key.key_name
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.allow_ssh.name]
-  user_data       =file("${path.module}/install-k8s.sh")
-  
+  count         = var.instance_count
+  ami           = var.ec2_ami_id
+  key_name      = data.aws_key_pair.my_ec2_key.key_name
+  instance_type = var.instance_type
+
+  security_groups = [data.aws_security_group.allow_ssh.name]
+
+  user_data = file("${path.module}/install-k8s.sh")
+
   root_block_device {
     volume_size = var.root_block_device_volume_size
     volume_type = "gp3"
   }
+
   tags = {
-    Name       = "k8s-ec2-instance"
-}
+    Name = "k8s-ec2-instance"
+  }
 }
